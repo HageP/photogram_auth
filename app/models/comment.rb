494e4f2e4eb8fa3f5a5d58ee18b91f140e9f5ed1
HageP <phage@chicagobooth.edu>
2016-11-26 01:28:37 -0600

    many changes. all functionality there but not rake grading

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ActiveRecord::Base
     validates :photo_id, :presence => true
       validates :user_id, :presence => true
         validates :body, :presence => true
+
+        belongs_to :user
 end
